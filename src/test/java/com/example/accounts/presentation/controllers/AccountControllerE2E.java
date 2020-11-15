0512a6d9bef813c3ea4ec32eed796ac220cc9c5b
dbenitez-bcn <dbr.bcn@gmail.com>
2020-11-15 22:19:51 +0100

    updates e2e for accounts

--- src/test/java/com/example/accounts/presentation/controllers/AccountControllerE2E.java
+++ src/test/java/com/example/accounts/presentation/controllers/AccountControllerE2E.java
@@ -5,14 +5,25 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 class AccountControllerE2E extends ApplicationTestCase {
     @Test
-    void foo_fee() throws Exception {
+    void whenUserIntroducesWrongCredentialsShouldFail() throws Exception {
         JSONObject body = new JSONObject();
-        body.put("email", "admin@biblioteca.com");
+        body.put("email", "fake@biblioteca.com");
+        body.put("password", "wrongPassword1234");
+
+        assertRequestWithBody(GET, "/v1/login", body, 403);
+    }
+
+    @Test
+    void afterUserCreatesANewAccountShouldBeAbleToLogIn() throws Exception {
+        JSONObject body = new JSONObject();
+        body.put("email", "testerino@biblioteca.com");
         body.put("password", "password1234");
 
+        assertRequestWithBody(POST, "/v1/register", body, 200);
         assertRequestWithBody(GET, "/v1/login", body, 200);
     }
 }
